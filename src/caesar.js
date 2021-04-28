5ecfcae8d3e76b33f51b2f5aa11c029ffaa35252
djdenney <daniel@denneythe.dev>
2021-04-28 16:09:02 -0700

    cleaned up code

--- src/caesar.js
+++ src/caesar.js
@@ -1,37 +1,33 @@
-// Please refrain from tampering with the setup code provided here,
-// as the index.html and test files rely on this setup to work properly.
-// Only add code (e.g., helper methods, variables, etc.) within the scope
-// of the anonymous function on line 6
-
 const caesarModule = (function () {
-  // you can add any code you want within this function scope
-
-  function caesar(input, shift, encode = true) {
-    if (!shift || shift === 0 || shift < -25 || shift > 25) {
-      return false
+    // a function that will accept a string argument, then
+    // shift all of the characters of that string alphabetically
+    // based on the shift argument, this can be used to encode
+    // and decode an input based on the encode argument
+    function caesar(input, shift, encode = true) {
+        if (!shift || shift === 0 || shift < -25 || shift > 25) {
+            return false
+        }
+        const alphaCharacters = 'abcdefghijklmnopqrstuvwxyz'.split('')
+        const inputCharacters = input.toLowerCase().split('')
+        let result = inputCharacters.map((inputCharacter) => {
+            const startingPosition = alphaCharacters.indexOf(inputCharacter)
+            let endingPosition = startingPosition
+            !encode ? endingPosition = endingPosition - shift : endingPosition = endingPosition + shift    
+            if (!alphaCharacters.includes(inputCharacter)) {
+                return inputCharacter
+            }
+            if (endingPosition > 25) {
+                endingPosition += -26
+            } else if (endingPosition < 0) {
+                endingPosition += 26
+            }
+            return alphaCharacters[endingPosition]
+        })
+        return result.join('')
     }
-    const alphaCharacters = 'abcdefghijklmnopqrstuvwxyz'.split('');
-    const inputCharacters = input.toLowerCase().split('');
-    let result = inputCharacters.map((inputCharacter) => {
-      const startingPosition = alphaCharacters.indexOf(inputCharacter);
-      let endingPosition = startingPosition;
-      !encode ? endingPosition = endingPosition - shift : endingPosition = endingPosition + shift;    
-      if (!alphaCharacters.includes(inputCharacter)) {
-        return inputCharacter;
-      }
-      if (endingPosition > 25) {
-        endingPosition += -26;
-      } else if (endingPosition < 0) {
-        endingPosition += 26;
-      }
-      return alphaCharacters[endingPosition];
-    });
-    return result.join('');
-  }
+    return {
+        caesar,
+    }
+})()
 
-  return {
-    caesar,
-  };
-})();
-
-module.exports = caesarModule.caesar;
+module.exports = caesarModule.caesar
--- src/substitution.js
+++ src/substitution.js
@@ -1,58 +1,54 @@
-// Please refrain from tampering with the setup code provided here,
-// as the index.html and test files rely on this setup to work properly.
-// Only add code (e.g., helper methods, variables, etc.) within the scope
-// of the anonymous function on line 6
-
 const substitutionModule = (function () {
-  // you can add any code you want within this function scope
-
-  function substitution(input, alphabet, encode = true) {
-    if (!alphabet) {
-      return false;
-    }
-    const alphaCharacters = 'abcdefghijklmnopqrstuvwxyz'.split('');
-    const inputAlphaCharacters = alphabet.split('');
-    if (
-      alphabet.length != 26 || 
-      inputAlphaCharacters.filter((character, index) => inputAlphaCharacters.indexOf(character) != index).length > 0
-      ) {
-      return false;
-    }
-    const inputCharacters = input.toLowerCase().split('');
-    let cypher = {};
-    let outputCharacters = []
-    alphaCharacters.forEach((alphaCharacter, i) => cypher[alphaCharacter] = inputAlphaCharacters[i]);
-    if (!encode) {
-      outputCharacters = inputCharacters.map((character) => {
-        if (character === ' ') {
-          return character;
-        } else {
-          for (alpha in cypher) {
-            if (cypher[alpha] === character) {
-              return alpha;
-            }
-          }
+    // a function that will substitute input string
+    // with corresponding characters from user-defined
+    // alphabet, with the ability to encode or decode
+    function substitution(input, alphabet, encode = true) {
+        if (!alphabet) {
+            return false
         }
-      });
-    } else {
-      outputCharacters = inputCharacters.map((character) => {
-        if (character === ' ') {
-          return character;
+        const alphaCharacters = 'abcdefghijklmnopqrstuvwxyz'.split('')
+        const inputAlphaCharacters = alphabet.split('')
+        if (
+            alphabet.length != 26 || 
+            inputAlphaCharacters.filter((character, index) => inputAlphaCharacters.indexOf(character) != index).length > 0
+        ) {
+            return false
+        }
+        const inputCharacters = input.toLowerCase().split('')
+        let cypher = {}
+        let outputCharacters = []
+        alphaCharacters.forEach((alphaCharacter, i) => cypher[alphaCharacter] = inputAlphaCharacters[i])
+        if (!encode) {
+            outputCharacters = inputCharacters.map((character) => {
+                if (character === ' ') {
+                    return character
+                } else {
+                    for (alpha in cypher) {
+                        if (cypher[alpha] === character) {
+                            return alpha
+                        }
+                    }
+                }
+            })
         } else {
-          for (alpha in cypher) {
-            if (alpha === character) {
-              return cypher[alpha];
-            }
-          }
-        }   
-      });
+            outputCharacters = inputCharacters.map((character) => {
+                if (character === ' ') {
+                    return character
+                } else {
+                    for (alpha in cypher) {
+                        if (alpha === character) {
+                            return cypher[alpha]
+                        }
+                    }
+                }   
+            })
+        }
+        console.log(outputCharacters.join(''))
+        return outputCharacters.join('')
     }
-    console.log(outputCharacters.join(''));
-    return outputCharacters.join('');
-  }
-  return {
-    substitution,
-  };
-})();
+    return {
+        substitution,
+    }
+})()
 
-module.exports = substitutionModule.substitution;
+module.exports = substitutionModule.substitution
